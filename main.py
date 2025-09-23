0c60cb8abf7d7888a4fed668ee5b35d945def4a0
Laura <laurapapa693@gmail.com>
2025-09-23 09:47:01 -0300

    add tabela criada

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import sqlite3
+
+conexao = sqlite3.connect("biblioteca.db")
+cursor = conexao.cursor()
+
+("""
+CREATE TABLE IF NOT EXISTS biblioteca(
+    id INTERGER PRIMARY KEY AUTOINCREMENT,
+    titulo TEXT NOT NULL,
+    autor TEXT NOT NULL,
+    ano INTERGER)
+ """)
+print("Tabela criada!")
+
